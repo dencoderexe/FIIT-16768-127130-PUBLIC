d5d2d8ab25ce99d34d692a85855ce60200da2933
Denis Danilov <dencoder.exe@gmail.com>
2026-03-08 23:36:33 +0100

    minor changes in app.py

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import dash
 from dash import Dash, html, dcc, Input, Output, State, callback, no_update, clientside_callback
 import dash_mantine_components as dmc
 from components.appshell import make_appshell, make_appshell_callbacks
+from components.file_picker import make_file_picker_callbacks
 
 app = Dash(use_pages=True, suppress_callback_exceptions=True)
 
 make_appshell_callbacks(app)
+make_file_picker_callbacks()
 
 app.layout = dmc.MantineProvider(
     children=[
