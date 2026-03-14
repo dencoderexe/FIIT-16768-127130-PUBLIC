6c4e42c015b71ed0d7e8805c956a9e1f3b8ffeae
Denis Danilov <dencoder.exe@gmail.com>
2026-03-14 23:31:50 +0100

    add: helper component

---
+++ components/helper.py
@@ -0,0 +1,27 @@
+import dash_mantine_components as dmc
+from dash_iconify import DashIconify
+
+def helper(help_message: str):
+    return dmc.HoverCard(
+        withArrow=True,
+        width=260,
+        shadow="xs",
+        position="top",
+        openDelay=150,
+        children=[
+            dmc.HoverCardTarget(
+                dmc.ActionIcon(
+                    DashIconify(icon="bi:question-circle", width=13),
+                    variant="subtle",
+                    size="xs",
+                )
+            ),
+            dmc.HoverCardDropdown(
+                dmc.Text(
+                    help_message,
+                    size="sm",
+                    style={"whiteSpace": "normal"},
+                )
+            ),
+        ],
+    )
