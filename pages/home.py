310210c1b8c8552a640e907f8168d14fc644576d
Denis Danilov <dencoder.exe@gmail.com>
2026-03-26 17:13:01 +0100

    add: home page

---
+++ pages/home.py
@@ -0,0 +1,14 @@
+import dash
+import dash_mantine_components as dmc
+
+dash.register_page(__name__, path="/")
+
+layout = dmc.Container(
+    [
+        dmc.Title("MSI Analyzer", order=1),
+        dmc.Space(h="md"),
+        dmc.Text("Welcome to the home page."),
+    ],
+    fluid=True,
+    p="md",
+)
