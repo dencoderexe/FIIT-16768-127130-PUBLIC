a2699a09bd2d441ae40842f26785d6540090be0f
Denis Danilov <dencoder.exe@gmail.com>
2026-04-09 18:42:54 +0200

    refactor: add categories for tools; add comments

--- components/appshell.py
+++ components/appshell.py
@@ -3,9 +3,13 @@ import dash_mantine_components as dmc
 from dash import Input, Output, State, callback, clientside_callback, html
 from dash_iconify import DashIconify
 
+# the main AppShell structure was inspired by the official Mantine example:
+# https://github.com/snehilvj/dmc-docs/blob/main/help_center/appshell/appshell_with_theme_switch.py
+
 logo = "/assets/fiit/PNG/STU-FIIT-nvf.png"
 
 def make_appshell(content):
+    # toggle between light and dark theme (client-side controlled)
     theme_toggle = dmc.Switch(
         offLabel=DashIconify(
             icon="radix-icons:sun", width=20, color= "var(--mantine-color-yellow-8)"
@@ -23,19 +27,23 @@ def make_appshell(content):
 
     return dmc.AppShell(
         [
+            # dummy Div used as a target for clientside theme switching
             html.Div(id="theme-dummy", style={"display": "none"}),
 
+            # header
             dmc.AppShellHeader(
                 dmc.Group(
                     [
                         dmc.Group(
                             [
+                                # burger menu for collapsing navbar on small screens
                                 dmc.Burger(
                                     id="burger",
                                     size="sm",
                                     hiddenFrom="sm",
                                     opened=False,
                                 ),
+                                # FIIT STU logo
                                 dmc.Image(src=logo, h=50, flex=0),
                                 dmc.Title("MSI Analyzer", c="blue"),
                             ]
@@ -48,28 +56,51 @@ def make_appshell(content):
                     px="md",
                 ),
             ),
+            # sidebar navigation
             dmc.AppShellNavbar(
                 id="navbar",
                 children=[
-                    "Navbar",
+                    # home page
                     dmc.NavLink(label="Home", href="/", leftSection=DashIconify(icon="bi:house-door-fill", height=16)),
+                    # tool pages
                     dmc.NavLink(
                         label="Tools",
                         childrenOffset=12,
                         leftSection=DashIconify(icon="bi:tools", height=16),
                         children=[
+                            dmc.Text(
+                                "Data processing",
+                                size="xs",
+                                c="dimmed",
+                                fw=700,
+                                tt="uppercase",
+                            ),
+                            dmc.Divider(),
+
                             dmc.NavLink(label="Samtools", href="/samtools"),
                             dmc.NavLink(label="RepeatFinder", href="/repeatfinder"),
+
+                            dmc.Text(
+                                "MSI analysis", 
+                                size="xs", 
+                                c="dimmed", 
+                                fw=700, 
+                                tt="uppercase"),
+                            dmc.Divider(),
+
                             dmc.NavLink(label="MSIsensor", href="/msisensor"),
                             dmc.NavLink(label="MSIsensor2", href="/msisensor2"),
                             dmc.NavLink(label="MSIsensor-pro", href="/msisensor-pro"),
                             dmc.NavLink(label="MANTIS", href="/mantis"),
                         ],
                     ),
+                    # jobs monitoring page
                     dmc.NavLink(label="Jobs", href="/jobs", leftSection=DashIconify(icon="bi:terminal-fill", height=16)),
                 ],
                 p="md",
             ),
+
+            # main content area
             dmc.AppShellMain(content),
         ],
         header={"height": 60},
@@ -89,10 +120,12 @@ def make_appshell_callbacks():
         State("appshell", "navbar"),
     )
     def navbar_is_open(opened, navbar):
+        # toggle navbar visibility on mobile devices
         navbar["collapsed"] = {"mobile": not opened}
         return navbar
 
 
+    # client-side callback to toggle Mantine color scheme light/dark
     clientside_callback(
         """
         function(switchOn) {
