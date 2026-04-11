917e3ef1961f9a3c7854bc08f898c3d54702811f
Denis Danilov <dencoder.exe@gmail.com>
2026-04-12 01:57:22 +0200

    refactor: add extensions variables

--- configs/paths.py
+++ configs/paths.py
@@ -5,3 +5,12 @@ tools_path  = "/home/danilovd/tools/"
 
 job_output_excluded_extensions = {".log", ".json", ".hist",}
 job_output_excluded_files = {}
+
+# EXTENSIONS
+FASTA_EXT = (".fasta", ".fas", ".fa", ".fna", ".ffn", ".faa", ".mpfa", ".frn",)
+FASTA_IDX_EXT = (".fai",)
+BAM_EXT = (".bam",)
+BAM_IDX_EXT = (".bai",)
+MICROSAT_LIST_EXT = (".microsatellite.list",)
+MICROSAT_LIST_PRO_EXT = (".microsatellite.list.pro",)
+BED_EXT = (".bed",)
